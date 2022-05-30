54c8618c951fc311c727b152ff4c17cac4e905fd
MoritaDaiki <tamo.d.tamo@gmail.com>
2022-05-30 19:49:50 +0900

    simple sample

---
+++ SamplingMoire/CMakeFiles/SamplingMoire.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SamplingMoire.dir/src/main.cpp.o"
+  "SamplingMoire"
+  "SamplingMoire.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SamplingMoire.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
